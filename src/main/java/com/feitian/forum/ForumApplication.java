ced5e8bc6cc181318a1b82b67427496ce19bd6e9
Wingflyinsky <904598749@qq.com>
2020-11-10 15:34:49 +0800

    和layui框架下的登录界面初步连接

--- src/main/java/com/feitian/forum/ForumApplication.java
+++ src/main/java/com/feitian/forum/ForumApplication.java
@@ -1,9 +1,11 @@
 package com.feitian.forum;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.feitian.forum.mapper")
 public class ForumApplication {
 
     public static void main(String[] args) {
